d4f4727dbf5c4dbfc0532f9a12a5bcf4c3a9d3da
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-05-01 21:17:59 -0300

    fix: pop-up syle

--- buttonoption.js
+++ buttonoption.js
@@ -1,21 +1,7 @@
-const popup = document.createElement("div");
-popup.setAttribute("id", "popup");
-popup.style.display = "none";
-popup.style.padding = "10px";
-popup.style.fontSize = "16px";
-popup.style.borderRadius = "5px";
-popup.style.position = "absolute";
-popup.style.color = "hsl(212, 23%, 69%)";
-popup.style.background = "hsl(217, 19%, 35%)";
-popup.style.boxShadow = "2px 2px 5px rgba(0, 0, 0, 0.3)";
-document.body.appendChild(popup);
+const popup = document.getElementById('popup')
 
 function showOptions(event) {
-  popup.innerHTML =
-    "<button onclick='option1()'>Opção 1</button><button onclick='option2()'>Opção 2</button><button onclick='option3()'>Opção 3</button>";
-  popup.style.top = event.clientY + "px";
-  popup.style.left = event.clientX + "px";
-  popup.style.display = "block";
+  popup.style.display = "flex";
 }
 
 function option1() {
